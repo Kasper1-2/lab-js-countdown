bcf678af39865c25b9317d55180e67fc540a6722
Diego Cisneros <dcisnerosg28@gmail.com>
2024-10-25 14:36:22 +0200

    2nd iteration

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-document.getElementById(start-btn).addEventListener("click", function() {
+document.getElementById("start-btn").addEventListener("click", function() {
   console.log("start button clicked!");
   startCountdown();
-}
+});
 
 
 
@@ -18,16 +18,16 @@ document.getElementById(start-btn).addEventListener("click", function() {
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-  let countdown = seconds;
+  let countdown = DURATION;
   const interval = setInterval(() => {
-  document.getElementById(time)
+  document.getElementById("time").innerText = countdown;
   console.log(countdown);
   countdown--;
 
   if (countdown === 0) {
     clearInterval(interval);
   }
-  })
+  }, 1000);
 }
 
 startCountdown();
